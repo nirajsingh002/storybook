f22f555a59be7a0b096b3523ad6d0a044f398bf0
Niraj Kumar <niraj.kumar2@publicissapient.com>
2021-02-27 12:59:55 +0530

    Added marketing storybook

---
+++ foundation-storybook/.storybook/preview.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import fetch from 'node-fetch';
+import '../src/stories/assets/css/base.css';
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+}
